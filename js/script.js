fe67fdf88096c05587c2b4115e77a127ad081148
Troy Becksvoort <becksvoort09@gmail.com>
2018-02-22 16:55:57 -0800

    basic layout

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var userItem = document.querySelector("#userItem");
+var addBtn = document.querySelector("#addBtn");
+var main = document.querySelector("#main");
+var congrats = document.querySelector("#pic");
+var done = 0;
+addBtn.addEventListener("click", function(){
+  if (userItem.value.length !== 0) {
+    var newItem = document.createElement("h2");
+    var newItemBox = document.createElement("div");
+    var newButton = document.createElement("button");
+    var newClear = document.createElement("button");
+    newItem.textContent = userItem.value;
+    newButton.textContent = "Task Completed";
+    newClear.textContent = "Clear Task";
+    newItemBox.classList.add("item-box");
+    newButton.classList.add("completed");
+    newClear.classList.add("completed");
+    newItemBox.appendChild(newButton);
+    newItemBox.appendChild(newItem);
+    newItemBox.appendChild(newClear);
+    main.appendChild(newItemBox);
+    newButton.onclick = success;
+    newClear.onclick = clear;
+  } else {
+    alert("please enter a task");
+  }
+});
+
+function success() {
+  if (done == 0) {
+    done += 1;
+    this.nextSibling.classList.add("done");
+  } else {
+    done -= 1;
+    this.nextSibling.classList.remove("done");
+  }
+}
+
+function clear() {
+  this.parentNode.style.display = "none";
+}
